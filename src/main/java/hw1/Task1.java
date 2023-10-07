a1e304b5679dc4136154b548e59711dea612f8d1
alexvic-che <alexvic.che@gmail.com>
2023-10-08 00:19:27 +0300

    First hw1 commit

--- src/main/java/hw1/Task1.java
+++ src/main/java/hw1/Task1.java
@@ -1,4 +1,39 @@
 package hw1;
 
 public class Task1 {
+    public static int getVideoLengthInSeconds(String timeString) {
+        // Разделить строку на минуты и секунды
+        String[] parts = timeString.split(":");
+
+        // Убедиться, что есть две части (минуты и секунды)
+        if (parts.length != 2) {
+            return -1; // Некорректный формат
+        }
+
+        try {
+            int minutes = Integer.parseInt(parts[0]);
+            int seconds = Integer.parseInt(parts[1]);
+
+            // Проверить, что секунды меньше 60
+            if (seconds >= 60) {
+                return -1; // Некорректный формат
+            }
+
+            // Вычислить общую длину видео в секундах
+            return minutes * 60 + seconds;
+        } catch (NumberFormatException e) {
+            return -1; // Некорректный формат (не удалось преобразовать в числа)
+        }
+    }
+
+    public static void main(String[] args) {
+        String timeString = "10:60";
+        int videoLengthInSeconds = getVideoLengthInSeconds(timeString);
+
+        if (videoLengthInSeconds != -1) {
+            System.out.println("Общая длина видео в секундах: " + videoLengthInSeconds);
+        } else {
+            System.out.println("-1.");
+        }
+    }
 }
---
+++ src/test/java/hw1/VideoLengthConverterTest.java
@@ -0,0 +1,35 @@
+package hw1;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class VideoLengthConverterTest {
+
+    // Тест для корректного времени
+    @Test
+    public void testValidTimeString() {
+        String timeString = "12:44";
+        int expectedSeconds = 12 * 60 + 44;
+        int actualSeconds = Task1.getVideoLengthInSeconds(timeString);
+        assertEquals(expectedSeconds, actualSeconds);
+    }
+    // Тест для некорректного времени (секунды >= 60)
+    @Test
+    public void testInvalidTimeString() {
+        String invalidTimeString = "12:60"; // Некорректное время (секунды >= 60)
+        int actualSeconds = Task1.getVideoLengthInSeconds(invalidTimeString);
+        assertEquals(-1, actualSeconds);
+    }
+    // Тест для некорректного формата времени (больше двух частей)
+    @Test
+    public void testInvalidFormat() {
+        String invalidFormatString = "12:44:00"; // Некорректный формат времени (больше двух частей)
+        int actualSeconds = Task1.getVideoLengthInSeconds(invalidFormatString);
+        assertEquals(-1, actualSeconds);
+    }
+    // Тест для нечислового ввода
+    @Test
+    public void testNonNumericInput() {
+        String nonNumericString = "abc:def"; // Не числовой ввод
+        int actualSeconds = Task1.getVideoLengthInSeconds(nonNumericString);
+        assertEquals(-1, actualSeconds);
+    }
+}
