31cf8f4e56ce6c9efbd4e6038bb75e71bdd2fa00
alexvic-che <alexvic.che@gmail.com>
2023-10-07 23:05:25 +0300

    First hw1 commit

---
+++ src/main/java/hw0/Task0.java
@@ -0,0 +1,14 @@
+package hw0;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+public class Task0 {
+    private final static Logger LOGGER = LogManager.getLogger();
+
+    public  static  void sayHello(){
+        LOGGER.info("Hello and welcome!");
+    }
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+}
