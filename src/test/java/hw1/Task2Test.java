d4f4bb4fb612454b95c0bae3f80c72fb4cbef264
alexvic-che <alexvic.che@gmail.com>
2023-10-27 13:22:54 +0300

    Final Hw3

--- src/test/java/hw1/Task2Test.java
+++ src/test/java/hw1/Task2Test.java
@@ -7,21 +7,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Task2Test {
     @Test
     public void testCountDigitsPositiveNumber() {
-        assertEquals(4,Task2.countDigits(4666));
-        assertEquals(3,Task2.countDigits(544));
-        assertEquals(1,Task2.countDigits(0));
-        assertEquals(5,Task2.countDigits(12345));
+        assertEquals(4, Task2.countDigits(4666));
+        assertEquals(3, Task2.countDigits(544));
+        assertEquals(1, Task2.countDigits(0));
+        assertEquals(5, Task2.countDigits(12345));
     }
 
     @Test
     public void testCountDigitsNegativeNumber() {
-        assertEquals(4,Task2.countDigits(-4666));
-        assertEquals(3,Task2.countDigits(-544));
-        assertEquals(5,Task2.countDigits(-12345));
+        assertEquals(4, Task2.countDigits(-4666));
+        assertEquals(3, Task2.countDigits(-544));
+        assertEquals(5, Task2.countDigits(-12345));
     }
 
     @Test
     public void testCountDigitsZero() {
-        assertEquals(1,Task2.countDigits(0));
+        assertEquals(1, Task2.countDigits(0));
     }
 }
